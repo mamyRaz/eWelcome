b5a68c6711cd04de0b740e750aa88f6141cfc3d4
angelembayangseck09@gmail.com <angelembayangseck09@gmail.com>
2019-12-09 09:10:31 +0000

    Premiere edition des interfaces

--- eWelcome/src/main/java/org/ewelcome/controller/URLMapping.java
+++ eWelcome/src/main/java/org/ewelcome/controller/URLMapping.java
@@ -2,12 +2,55 @@ package org.ewelcome.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class URLMapping {
 
 	@GetMapping("/")
-	public String get() {
+	public String home() {
 		return "index";
 	}
+	@GetMapping("/connexion")
+	public String connexion() {
+		return "connexion";
+	}
+	
+	@GetMapping("/listeUser")
+	public String listeUser() {
+		return "admin/listeUser";
+	}
+	
+	@GetMapping("/ajoutUser")
+	public String ajoutUser() {
+		return "admin/ajoutUser";
+	}
+	@GetMapping("/planningSecretaire")
+	public String planningSecretaire() {
+		return "secretaire/planningSecretaire";
+	}
+	@GetMapping("/planningAgent")
+	public String planningAgent() {
+		return "agent/planningAgent";
+	}
+	
+	@PostMapping("/accesApplicationSurPlace")
+	public String accesApplicationSurPlace() {
+		return "visiteur/accesApplicationSurPlace";
+	}
+	
+	@GetMapping("/formulairePriseRDV")
+	public String formulairePriseRDV() {
+		return "visiteur/formulairePriseRDV";
+	}
+	@GetMapping("/impressionBadgeMDPWifi")
+	public String impressionBadgeMDPWifi() {
+		return "visiteur/impressionBadgeMDPWifi";
+	}
+	
+	@GetMapping("/planningPriseRDV")
+	public String planningPriseRDV() {
+		return "visiteur/planningPriseRDV";
+	}
+	
 }
